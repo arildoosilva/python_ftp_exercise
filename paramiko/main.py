55526c959f3704e47547caaa72c14b1735c068dc
Arildo Oliveira <arildo.o.s@outlook.com>
2015-04-25 14:35:58 -0300

    Updated main.py

--- paramiko/main.py
+++ paramiko/main.py
@@ -1,68 +1,87 @@
 # -*- coding: utf-8 -*-
 # python3.4
 
+import getpass
+import sys, getopt
+import ftp
 import os
-import paramiko
-import time
 from threading import Thread
 
-os.system('cls' if os.name == 'nt' else 'clear')
-
 # vars
-# first connection
-server1 = ''
-user1 = ''
-passwd1 = ''
-dir1 = ''
-port1 = 22
-# second connection
-server2 = ''
-user2 = ''
-passwd2 = ''
-dir2 = ''
-port2 = 22
-
-def generate_hash_file(server, user, passwd, dir):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+server1 = None
+user1 = None
+passwd1 = None
+dir1 = None
+server2 = None
+user2 = None
+passwd2 = None
+dir2 = None
+    
+def main(argv):
+    help = ('Uses python 3.4\n\n'
+            'Usage: python main.py <server1> <username1> <directory1> <server2> <username2> <directory2> <password1> <password2>\n\n'
+            'Necessary modules: getpass, sys, getopt, os, threading, paramiko, time\n\n'
+            'SFTP and SSH are required')
+    
+    #prints the help text when -h or --help is used
     try:
-        ssh.connect(server, username = user, password = passwd)
-    except paramiko.SSHException:
-        print('Connection Failed')
-        quit()
-    print('Connected to', server)
-    command = ('find '+str(dir)+' -type f -exec sha256sum {} \; > /home/'+str(user)+'/$(hostname)-$(date +%s).txt')
-    stdin,stdout,stderr = ssh.exec_command(command, get_pty = True)
-    exit_status = stdout.channel.recv_exit_status()
-    print('Checksum file generated for', dir, '-', ('OK' if exit_status == 0 else ('Error generating for', dir)))
-    #for line in stdout.readlines():
-    #print(line.strip())
-print('Finished. Disconnecting SSH from', server)
-    ssh.close()
+        opts, args = getopt.getopt(argv,'h',['help'])
+    except getopt.GetoptError:
+        print(help)
+        sys.exit(2)
 
-def get_hash_file(server, user, passwd, port):
-    t = paramiko.Transport((server, port))
-    try:
-        t.connect(username = user, password = passwd)
-    except paramiko.SSHException:
-        print('Connection Failed')
-        quit()
-    sftp = paramiko.SFTPClient.from_transport(t)
-    sftp.get(server+'.txt', server+'.txt')
-    t.close()
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print(help)
+            sys.exit()
 
-#generate_hash_file(server1, user1, passwd1, dir1)
-#generate_hash_file(server2, user2, passwd2, dir2)
-#get_hash_file(server1, user1, passwd1, port1)
-#get_hash_file(server2, user2, passwd2, port2)
+    global server1
+    global user1
+    global passwd1
+    global dir1
+    global server2
+    global user2
+    global passwd2
+    global dir2
 
-t1 = Thread(target=generate_hash_file, args=(server1, user1, passwd1, dir1))
-t1.start()
-time.sleep(2)
-t2 = Thread(target=generate_hash_file, args=(server2, user2, passwd2, dir2))
-t2.start()
-t1.join()
-t2.join()
+    if len(sys.argv) > 1: server1 = sys.argv[1]
+    if len(sys.argv) > 2: user1 = sys.argv[2]
+    if len(sys.argv) > 3: dir1 = sys.argv[3]
+    if len(sys.argv) > 4: server2 = sys.argv[4]
+    if len(sys.argv) > 5: user2 = sys.argv[5]
+    if len(sys.argv) > 6: dir2 = sys.argv[6]
+    if len(sys.argv) > 8: passwd1 = sys.argv[7]
+    if len(sys.argv) > 9: passwd2 = sys.argv[8]
 
-get_hash_file(server1, user1, passwd1, port1)
-get_hash_file(server2, user2, passwd2, port2)
+    if server1 == None: server1 = input('Server 1: ')
+    if user1 == None: user1 = input('Username for server '+server1+': ')
+    if dir1 == None: dir1 = input('Directory: ')
+    if passwd1 == None:
+        print('Enter the password for server', server1)
+        passwd1 = getpass.getpass('Password: ', None)
+        
+    if server2 == None: server2 = input('\nServer 2: ')
+    if user2 == None: user2 = input('Username for server '+server2+': ')
+    if dir2 == None: dir2 = input('Directory: ')
+    if passwd2 == None:
+        print('Enter the password for server', server2)
+        passwd2 = getpass.getpass('Password: ', None)
+    
+if __name__ == "__main__":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    main(sys.argv[1:])
+    
+    t1 = Thread(target=ftp.generate_hash_file, args=(server1, user1, passwd1, dir1))
+    t1.start()
+    
+    time.sleep(2)
+    
+    t2 = Thread(target=ftp.generate_hash_file, args=(server2, user2, passwd2, dir2))
+    t2.start()
+    
+    t1.join()
+    t2.join()
+
+    ftp.get_hash_file(server1, user1, passwd1, 22)
+    ftp.get_hash_file(server2, user2, passwd2, 22)
