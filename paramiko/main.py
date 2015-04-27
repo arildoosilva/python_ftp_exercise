dc9c79835132c689fecc321fa33b1dc3acf5c73b
Arildo Oliveira <arildo.o.s@outlook.com>
2015-04-27 12:44:01 -0300

    Update main.py

--- paramiko/main.py
+++ paramiko/main.py
@@ -48,47 +48,28 @@ def main(argv):
     global passwd2
     global dir2
 
-    if len(sys.argv) > 0:
+    if len(sys.argv) < 7:
+        print('Missing parameters\n')
+        print(help)
+        sys.exit(2)
+    else:
         server1 = sys.argv[1]
-    if len(sys.argv) >= 1:
         user1 = sys.argv[2]
-    if len(sys.argv) >= 2:
         dir1 = sys.argv[3]
-    if len(sys.argv) >= 3:
         server2 = sys.argv[4]
-    if len(sys.argv) >= 4:
         user2 = sys.argv[5]
-    if len(sys.argv) >= 5:
         dir2 = sys.argv[6]
-    if len(sys.argv) >= 6:
-        passwd1 = sys.argv[7]
-    if len(sys.argv) >= 7:
-        passwd2 = sys.argv[8]
 
-    if server1 is None:
-        server1 = input('Server 1: ')
-    if user1 is None:
-        user1 = input('Username for server '+server1+': ')
-    if dir1 is None:
-        dir1 = input('Directory: ')
-    if passwd1 is None:
-        print('Enter the password for server-1', server1)
-        passwd1 = getpass.getpass('Password: ', None)
+    print('Enter the password for server-1', server1)
+    passwd1 = getpass.getpass('Password: ', None)
 
-    if server2 is None:
-        server2 = input('\nServer 2: ')
-    if user2 is None:
-        user2 = input('Username for server '+server2+': ')
-    if dir2 is None:
-        dir2 = input('Directory: ')
-    if passwd2 is None:
-        print('Enter the password for server-2', server2)
-        passwd2 = getpass.getpass('Password: ', None)
+    print('Enter the password for server-2', server2)
+    passwd2 = getpass.getpass('Password: ', None)
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    main(sys.argv[1:])
+    main(sys.argv[0:])
 
     t1 = Thread(target=ftp.generate_hash_file, args=(server1, user1, passwd1, dir1))
     t1.start()
