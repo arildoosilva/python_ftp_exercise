cb9e3b15cb294490e8211268f1fa11fd51a819cb
Arildo Oliveira <arildo.o.s@outlook.com>
2015-04-30 13:26:59 -0300

    Update main.py

--- paramiko/main.py
+++ paramiko/main.py
@@ -11,10 +11,12 @@ from threading import Thread
 
 # vars
 server1 = None
+port1 = None
 user1 = None
 passwd1 = None
 dir1 = None
 server2 = None
+port2 = None
 user2 = None
 passwd2 = None
 dir2 = None
@@ -22,8 +24,8 @@ dir2 = None
 
 def main(argv):
     help = ('Uses python 3.4\n\n'
-            'Usage: python main.py <server1> <username1> <directory1> '
-            '<server2> <username2> <directory2> <password1> <password2>\n\n'
+            'Usage: python main.py <server1> <port1> <username1> <directory1> '
+            '<server2> <port2> <username2> <directory2> <password1> <password2>\n\n'
             'Necessary modules: getpass, sys, getopt, os, threading, paramiko, time\n\n'
             'SFTP and SSH are required')
 
@@ -40,43 +42,51 @@ def main(argv):
             sys.exit()
 
     global server1
+    global port1
     global user1
     global passwd1
     global dir1
     global server2
+    global port2
     global user2
     global passwd2
     global dir2
 
-    if len(sys.argv) < 7:
+    if len(sys.argv) < 9:
         print('Missing parameters\n')
         print(help)
         sys.exit(2)
     else:
         server1 = sys.argv[1]
-        user1 = sys.argv[2]
-        dir1 = sys.argv[3]
-        server2 = sys.argv[4]
-        user2 = sys.argv[5]
-        dir2 = sys.argv[6]
-
-    print('Enter the password for server-1', server1)
-    passwd1 = getpass.getpass('Password: ', None)
-
-    print('Enter the password for server-2', server2)
-    passwd2 = getpass.getpass('Password: ', None)
+        port1 = sys.argv[2]
+        user1 = sys.argv[3]
+        dir1 = sys.argv[4]
+        server2 = sys.argv[5]
+        port2 = sys.argv[6]
+        user2 = sys.argv[7]
+        dir2 = sys.argv[8]
+        if len(sys.argv) < 10:
+            print('Enter the password for server-1', server1)
+            passwd1 = getpass.getpass('Password: ', None)
+        else:
+            passwd1 = sys.argv[9]
+        if len(sys.argv) < 11:
+            print('Enter the password for server-2', server2)
+            passwd2 = getpass.getpass('Password: ', None)
+        else:
+            passwd2 = sys.argv[10]
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
 
     main(sys.argv[0:])
 
-    t1 = Thread(target=ftp.generate_hash_file, args=(server1, user1, passwd1, dir1))
+    t1 = Thread(target=ftp.generate_hash_file, args=(server1, user1, passwd1, dir1, 22))  # 44 se for para o dm1
     t1.start()
 
     time.sleep(2)
 
-    t2 = Thread(target=ftp.generate_hash_file, args=(server2, user2, passwd2, dir2))
+    t2 = Thread(target=ftp.generate_hash_file, args=(server2, user2, passwd2, dir2, 22))
     t2.start()
 
     t1.join()
